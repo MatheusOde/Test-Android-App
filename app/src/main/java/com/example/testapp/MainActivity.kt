8859ef1c935049f0f068a62015f24ab8b61f122b
MatheusOde <matheusode@gmail.com>
2024-03-05 21:35:21 -0300

    First commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.testapp
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testapp.ui.theme.TestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier){
+    Text(
+        text = message,
+        fontSize = 90.sp,
+        lineHeight = 116.sp,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TestAppPreview(){
+    TestAppTheme {
+        GreetingText(message = "Happy Birthday Matheus!")
+    }
+}
